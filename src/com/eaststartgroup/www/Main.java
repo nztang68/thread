93dff9081b7ab47d04b19a6b685ad26934968254
Tom, TANG Nan Zeng <nztang68@hotmail.com>
2017-10-13 06:28:29 +0800

    Initial commit

---
+++ src/com/eaststartgroup/www/Main.java
@@ -0,0 +1,11 @@
+package com.eaststartgroup.www;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+        Thread thread1 = new ThreadObject();
+
+        thread1.start();
+    }
+}
---
+++ src/com/eaststartgroup/www/ThreadObject.java
@@ -0,0 +1,4 @@
+package com.eaststartgroup.www;
+
+public class ThreadObject extends Thread {
+}
