e43fc218fac228ad8c7e457dded4fc1f1b7ae325
Tom, TANG Nan Zeng <nztang68@hotmail.com>
2017-10-13 07:26:00 +0800

    201710130725.

--- src/com/eaststartgroup/www/ThreadObject.java
+++ src/com/eaststartgroup/www/ThreadObject.java
@@ -7,6 +7,12 @@ public class ThreadObject extends Thread {
         super.run();
         System.out.println("Hello from thread 1!");
         for (int i = 0; i < 20; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread has been interrupted.");
+                Thread.currentThread().interrupt();
+            }
             System.out.println("Count from thread 1 - " + i);
         }
     }
