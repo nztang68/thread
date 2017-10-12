4f349066e8219ed4a04c1e961d54d006a02df1bf
Tom, TANG Nan Zeng <nztang68@hotmail.com>
2017-10-13 07:36:47 +0800

    201710130736.

--- src/com/eaststartgroup/www/Main.java
+++ src/com/eaststartgroup/www/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 	// write your code here
         Thread thread1 = new ThreadObject();
+        Thread thread2 = new Thread(new ThreadAltObject());
 
         thread1.start();
+        thread2.start();
     }
 }
---
+++ src/com/eaststartgroup/www/ThreadAltObject.java
@@ -0,0 +1,8 @@
+package com.eaststartgroup.www;
+
+public class ThreadAltObject implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
