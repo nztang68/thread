2cd4158af8bbf36306987477af591bb5bb869ab5
Tom, TANG Nan Zeng <nztang68@hotmail.com>
2017-10-13 06:56:25 +0800

    201710130656.

--- src/com/eaststartgroup/www/ThreadObject.java
+++ src/com/eaststartgroup/www/ThreadObject.java
@@ -5,9 +5,9 @@ public class ThreadObject extends Thread {
     @Override
     public void run() {
         super.run();
-        System.out.println("Hello from thread!");
+        System.out.println("Hello from thread 1!");
         for (int i = 0; i < 20; i++) {
-            
+            System.out.println("Count from thread 1 - " + i);
         }
     }
 }
