e9a5a70562a01e041849727dedf1820677e84045
Tom, TANG Nan Zeng <nztang68@hotmail.com>
2017-10-13 06:52:12 +0800

    201710130651.

--- src/com/eaststartgroup/www/ThreadObject.java
+++ src/com/eaststartgroup/www/ThreadObject.java
@@ -1,4 +1,13 @@
 package com.eaststartgroup.www;
 
 public class ThreadObject extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("Hello from thread!");
+        for (int i = 0; i < 20; i++) {
+            
+        }
+    }
 }
