89b460e92550ee6d554f2ce643c935080c68cc67
Tom, TANG Nan Zeng <nztang68@hotmail.com>
2017-10-13 09:05:22 +0800

    201710130905.

--- src/com/eaststartgroup/www/ThreadAltObject.java
+++ src/com/eaststartgroup/www/ThreadAltObject.java
@@ -1,8 +1,32 @@
 package com.eaststartgroup.www;
 
 public class ThreadAltObject implements Runnable {
+/*
     @Override
     public void run() {
+        System.out.println("Hello from thread 2!");
+        for (int i = 0; i < 20; i++) {
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Count from thread 2 - " + i);
+        }
+    }
+*/
 
+    @Override
+    public void run() {
+        System.out.println("Hello from thread 2!");
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            if (i%10_000_000 == 0) {
+                System.out.println("Count from thread 2 - " + i);
+                if (Thread.interrupted()) {
+                    System.out.println("Thread 2 has been interrupted.");
+                    break;
+                }
+            }
+        }
     }
 }
